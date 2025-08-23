ea736d80dc7019559fe383008a781851c41f63a7
moss-3c <117598547+moss-cc@users.noreply.github.com>
2025-08-23 16:13:07 -0700

    Allow books to be deleted

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function addBookToLib(title, author, pages, notes, read) {
     return book;
 }
 function displayLib () {
-
     //loops through the array and displays each book on the page
     myLib.forEach((book) => displayBook(book));
 }
@@ -27,16 +26,36 @@ function displayBook(book) {
     let properties = Object.keys(book);
     let content;
     //keys: id, title [1], author, pages, notes, read [5]
-     for (let i = 1; i <= 5 ; i++) {
+     for (let i = 1; i <= 5 ; i++) { //skip id key, don't want it in card
          content = card.querySelector(`.${properties[i]}`);
          content.textContent = book[properties[i]];
      }
      card.querySelector('.card').setAttribute("id", book.id);
+     card.querySelector('.delete').addEventListener('click', (e) => {
+        const id = e.target.parentNode.getAttribute('id');
+        deleteBook(id);
+        deleteCard(id);
+     });
+
+
     let body = document.querySelector('.container');
     body.appendChild(card);
 }
 
-//opening and closing the form
+function deleteBook(id) {
+    //find book with the id
+    //remove from lib array
+   myLib.splice(
+            myLib.indexOf(
+                myLib.find((book) => book.id === id)
+            ),
+        1);
+}
+function deleteCard(id) {
+    //remove card from ui
+    document.getElementById(id).remove();
+}
+
 const dialog = document.querySelector('dialog');
 const addBtn = document.querySelector('.add-book');
 addBtn.addEventListener('click', () => {dialog.showModal()})
@@ -61,8 +80,9 @@ form.addEventListener("submit", (e) => {
     form.reset();
 })
 
-/**
- * remove book button
- * read status
- */
+
+//testing
 myLib[0] = new Book("lotr","tolkien","987", "good book", "true");
+myLib[1] = new Book("bad book","ff","987", "sucks", "true");
+displayBook(myLib[0]);
+displayBook(myLib[1]);
